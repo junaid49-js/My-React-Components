f776661352d4f840160e846bc9c0f75ff22a11a3
Junaid <156448521+junaid49-js@users.noreply.github.com>
2025-07-23 17:19:58 +0530

    Create LoadingComponent.jsx

---
+++ Table-TanStack/Table_with_API_Data_in_Chunks/src/Components/LoadingComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingComponent = () => {
+  return (
+    <div className='h-screen flex items-center justify-center'>Retrieving Data....</div>
+  )
+}
+
+export default LoadingComponent
